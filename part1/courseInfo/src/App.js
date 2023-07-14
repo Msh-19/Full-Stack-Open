bf15efe1566d0f8514f935e48e4b076dc7bfa135
Mohammed Shemim <mohammed.shemim@aastustudent.edu.et>
2023-07-14 21:55:33 +0300

    changed app.js, fixed issue with the courseinfo

--- part1/courseInfo/src/App.js
+++ part1/courseInfo/src/App.js
@@ -1,57 +1,63 @@
+import React from 'react';
+
 const App = () => {
-   const course = {
+  const course = {
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
       },
       {
         name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        exercises: 14,
+      },
+    ],
+  };
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{course}</h1>
-    </div>
-  )
-  }
-   const Parts = props =>(
-    <p>
-    {props.part.name} {props.part.exercises}
-    </p>
-   )
-  
-  const Content = (props) =>{
-    return(
+  const Header = (props) => {
+    return (
       <div>
-        <Parts part={props.parts[0]}  />
+        <h1>{props.course}</h1>
+      </div>
+    );
+  };
+
+  const Parts = (props) => (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+
+  const Content = (props) => {
+    return (
+      <div>
+        <Parts part={props.parts[0]} />
         <Parts part={props.parts[1]} />
         <Parts part={props.parts[2]} />
       </div>
-    )
-  }
-  const Total = props => (<p> Number of exercises 
-    {props.parts.reduce((total, part) => part.exercises + total,0)} </p>) 
- 
-  
+    );
+  };
+
+  const Total = (props) => (
+    <p>
+      Number of exercises{' '}
+      {props.parts.reduce((total, part) => part.exercises + total, 0)}
+    </p>
+  );
+
   return (
     <div>
       <Header course={course.name} />
 
       <Content parts={course.parts} />
-      <Total parts = {course.parts}/>
-      
+      <Total parts={course.parts} />
     </div>
-    )
-}
+  );
+};
 
-export default App
+export default App;
